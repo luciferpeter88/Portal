3abcc7f25c6317a4cacca12a5c650edf1533c1ec
luciferpeter88git config user.email kaszap.nagy.peter@citromail.hu git config user.name luciferpeter88git config --global user.email kaszap.nagy.peter@citromail.hu <luciferpeter88>
2022-12-18 12:12:55 -0500

    set up the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Department from "./pages/Department";
+import Register from "./pages/Register";
 function App() {
-  return <React.Fragment>Hello</React.Fragment>;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav />}>
+            <Route index element={<Home />} />
+            <Route path="branches" element={<Login />} />
+            <Route path="issues" element={<Department />} />
+            <Route path="originmaster" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
