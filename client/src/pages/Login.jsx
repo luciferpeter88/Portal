9f4cf197a4f7a03a3085a677861563d5b5a34e30
luciferpeter88git config user.email kaszap.nagy.peter@citromail.hu git config user.name luciferpeter88git config --global user.email kaszap.nagy.peter@citromail.hu <luciferpeter88>
2022-12-27 10:47:42 -0500

    login mobile ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <React.Fragment>
+      <div className="login">
+        <div className="loginPicture"></div>
+        <h2>Welcome Back</h2>
+        <form className="loginContainer">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </form>
+        <button className="registerButton " type="submit">
+          Sign In
+        </button>
+        <p>
+          Dont't I have an account? <span>Sign Up</span>
+        </p>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Login;
