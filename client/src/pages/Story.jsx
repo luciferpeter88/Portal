e50ca5dbf24862dd9c659fe453a9f64047b3c2db
luciferpeter88git config user.email kaszap.nagy.peter@citromail.hu git config user.name luciferpeter88git config --global user.email kaszap.nagy.peter@citromail.hu <luciferpeter88>
2022-12-29 12:33:11 -0500

    finished story ui

--- client/src/pages/Story.jsx
+++ client/src/pages/Story.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import YouTube from "react-youtube";
 import { useParams, useNavigate } from "react-router-dom";
 import { context } from "../components/Context";
 function Story() {
   const { state } = React.useContext(context);
-
   const navigate = useNavigate();
   const { storyName } = useParams();
+
   const opts = {
     height: "390",
-    width: "640",
+    width: "390",
     playerVars: {
       autoplay: 1,
     },
@@ -19,7 +19,7 @@ function Story() {
     <React.Fragment>
       <div className="storyContainer">
         <div className="storyPic">
-          <img src={state.defaultPicture} />
+          <img src={state.defaultPicture} alt={state.defaultPicture} />
         </div>
         <div className="storyVideo">
           <h2>{storyName}</h2>
